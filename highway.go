ab3813d3ae5c68c2b30621008b2f8849f0353fd5
0xbunyip <>
2019-10-01 15:19:52 +0700

    Clean up highway.go

--- highway.go
+++ highway.go
@@ -6,14 +6,13 @@ import (
 	logger "highway/customizelog"
 	"highway/p2p"
 	"highway/process"
-	_ "net/http/pprof"
 )
 
 func main() {
 	config := GetProxyConfig()
 	config.printConfig()
 
-	//process proxy stream
+	// Process proxy stream
 	proxyHost := p2p.NewHost(config.version, config.host, config.proxyPort, []byte(config.privateKey))
 	process.ProcessConnection(proxyHost)
 	err := common.InitGenesisCommitteeFromFile("keylist.json", common.NumberOfShard+1, common.CommitteeSize)
@@ -21,24 +20,11 @@ func main() {
 		return
 	}
 
-	// Pubsub
-	// proxyHost := p2p.NewHost(
-	// 	config.version,
-	// 	config.host,
-	// 	config.proxyPort,
-	// 	[]byte(config.privateKey),
-	// )
 	if err := process.InitPubSub(proxyHost.Host); err != nil {
 		logger.Error(err)
 		return
-	} else {
-		go process.GlobalPubsub.WatchingChain()
-		logger.Println("Init ok")
 	}
 
-	// go ProcessConnection(proxyHost)
-	// time.Sleep(1 * time.Second)
-	select {}
-	//web server
-	// StartMonitorServer(config.adminPort)
+	process.GlobalPubsub.WatchingChain()
+	logger.Println("Init ok")
 }
