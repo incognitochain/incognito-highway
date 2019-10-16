bec6af5e38d448e9d074ae805a402e22f642c386
0xakk0r0kamui <hy@autonomous.nyc>
2019-10-16 14:43:15 +0700

    Add  more struct

---
+++ process/define.go
@@ -0,0 +1,28 @@
+package process
+
+// type ChainState struct {
+// 	Timestamp     int64
+// 	Height        uint64
+// 	BlockHash     [32]byte
+// 	BestStateHash [32]byte
+// }
+
+// type CommitteeState map[string][]byte
+
+// // var AllPeerState map[byte]CommitteeState
+// // var currentAllPeerStateData []byte
+
+// // NetworkState contains all of chainstate of node, which in committee and connected to proxy
+// type NetworkState struct {
+// 	BeaconState map[string]ChainState          // map[<Committee Public Key>]Chainstate
+// 	ShardState  map[byte]map[string]ChainState // map[<ShardID>]map[<Committee Public Key>]Chainstate
+// }
+
+// var NWState NetworkState
+
+// // type ChainState struct {
+// // 	Timestamp     int64
+// // 	Height        uint64
+// // 	BlockHash     [32]byte
+// // 	BestStateHash [32]byte
+// // }
