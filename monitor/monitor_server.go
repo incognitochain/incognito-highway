c8fce11f70c6d29d68f33d91d4ef999ff5bd9eed
0xbunyip <baoduy@autonomous.nyc>
2019-11-22 19:03:00 +0700

    [WIP] Implementing monitor API

--- monitor/monitor_server.go
+++ monitor/monitor_server.go
@@ -1,19 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func StartMonitorServer(port int) {
-	//run http server
-	http.HandleFunc("/admin", handler)
+	// Run http server
+	m := &monitor{}
+	go m.start()
+	http.Handle("/monitor", m)
 	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
+		// TODO(@0xbunyip): change to logger and prevent os.Exit()
 		log.Fatalf("Error in ListenAndServe: %s", err)
 	}
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func (m *monitor) ServeHTTP(w ResponseWriter, r *Request) {
+	profile := &monitor{}
+	js, err := json.Marshal(profile)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func (m *monitor) start() {
+	for ; true; <-time.Tick(5 * time.Second) {
+	}
+}
+
+type monitor struct {
 }
