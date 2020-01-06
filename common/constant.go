e97e9aebf56d253eb837c4d3c0377ed6d1fef82a
0xbunyip <baoduy@autonomous.nyc>
2020-01-07 00:07:15 +0700

    Update constants

--- common/constant.go
+++ common/constant.go
@@ -13,17 +13,25 @@ const (
 	WaitingRole   = "waiting"
 	NormalNode    = ""
 
-	MaxCallDepth                = 2
-	MaxBlocksPerRequest         = 100
-	MaxTimePerRequest           = 10 * time.Second
-	MaxTimeKeepPeerState        = 90 * time.Second
-	MaxTimeKeepPubSubData       = 30 * time.Second
-	ChoosePeerBlockDelta        = 100
+	ChainClientKeepaliveTime    = 10 * time.Minute
+	ChainClientKeepaliveTimeout = 20 * time.Second
+	ChainClientDialTimeout      = 5 * time.Second
+
+	MaxCallDepth         = 2
+	ChoosePeerBlockDelta = 100
+	MaxBlocksPerRequest  = 100
+	MaxTimePerRequest    = 10 * time.Second
+
+	TimeIntervalPublishStates = 5 * time.Second
+	MaxTimeKeepPeerState      = 90 * time.Second
+	MaxTimeKeepPubSubData     = 30 * time.Second
+
 	RouteClientKeepaliveTime    = 20 * time.Minute
 	RouteClientKeepaliveTimeout = 20 * time.Second
+	RouteClientDialTimeout      = 5 * time.Second
 	RouteHighwayKeepaliveTime   = 40 * time.Second
-	BroadcastMsgEnlistTimestep  = 5 * time.Minute
-	TimeIntervalPublishStates   = 5 * time.Second
+
+	BroadcastMsgEnlistTimestep = 5 * time.Minute
 
 	DefaultRPCServerPort = 9330
 	DefaultHWPeerID      = "/ip4/127.0.0.1/tcp/9330/p2p/QmSPa4gxx6PRmoNRu6P2iFwEwmayaoLdR5By3i3MgM9gMv"
