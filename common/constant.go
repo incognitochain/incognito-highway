b472532255f5cdf27bd5d73120f97e2c3cce861e
0xbunyip <baoduy@autonomous.nyc>
2020-01-07 17:31:31 +0700

    Revert constants

--- common/constant.go
+++ common/constant.go
@@ -29,10 +29,10 @@ const (
 	RouteClientKeepaliveTime    = 20 * time.Minute
 	RouteClientKeepaliveTimeout = 20 * time.Second
 	RouteClientDialTimeout      = 5 * time.Second
-	RouteKeepConnectionTimestep = 1 * time.Second
-	RouteHighwayKeepaliveTime   = 3 * time.Second
+	RouteKeepConnectionTimestep = 40 * time.Second
+	RouteHighwayKeepaliveTime   = 40 * time.Second
 
-	BroadcastMsgEnlistTimestep = 2 * time.Second
+	BroadcastMsgEnlistTimestep = 5 * time.Minute
 
 	DefaultRPCServerPort = 9330
 	DefaultHWPeerID      = "/ip4/127.0.0.1/tcp/9330/p2p/QmSPa4gxx6PRmoNRu6P2iFwEwmayaoLdR5By3i3MgM9gMv"
