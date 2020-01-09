4473dae6a54968a5435328c72fff11aa6f6892ca
0xbunyip <baoduy@autonomous.nyc>
2020-01-09 15:03:21 +0700

    Test group peers

--- chain/client_test.go
+++ chain/client_test.go
@@ -2,10 +2,65 @@ package chain
 
 import (
 	"highway/chaindata"
+	"math/rand"
 	"testing"
 
+	peer "github.com/libp2p/go-libp2p-core/peer"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestGroupPeers(t *testing.T) {
+	// Connected, with blk
+	selfPeerID := peer.ID("abc")
+	blk := uint64(100)
+	a := []chaindata.PeerWithBlk{}
+	for i := 0; i < 3; i++ {
+		a = append(a, chaindata.PeerWithBlk{
+			HW:     selfPeerID,
+			Height: uint64(123),
+		})
+	}
+
+	// Not connected, with blk
+	b := []chaindata.PeerWithBlk{}
+	for i := 0; i < 5; i++ {
+		b = append(b, chaindata.PeerWithBlk{
+			Height: uint64(123),
+		})
+	}
+
+	// Connected, without blk
+	c := []chaindata.PeerWithBlk{}
+	for i := 0; i < 7; i++ {
+		c = append(c, chaindata.PeerWithBlk{
+			HW:     selfPeerID,
+			Height: uint64(45),
+		})
+	}
+
+	// Not connected, without blk
+	d := []chaindata.PeerWithBlk{}
+	for i := 0; i < 9; i++ {
+		d = append(d, chaindata.PeerWithBlk{
+			Height: uint64(45),
+		})
+	}
+
+	expected := [][]chaindata.PeerWithBlk{a, b, c, d}
+
+	peers := []chaindata.PeerWithBlk{}
+	peers = append(peers, a...)
+	peers = append(peers, b...)
+	peers = append(peers, c...)
+	peers = append(peers, d...)
+	rand.Shuffle(len(peers), func(i, j int) {
+		peers[i], peers[j] = peers[j], peers[i]
+	})
+
+	connectedPeers := []PeerInfo{PeerInfo{ID: peer.ID("")}, PeerInfo{ID: selfPeerID}}
+	groups := groupPeersByDistance(peers, blk, selfPeerID, connectedPeers)
+	assert.Equal(t, expected, groups)
+}
 
 func TestChoosePeerFromGroup(t *testing.T) {
 	buildGroup := func(height uint64) []chaindata.PeerWithBlk {
