2d1d5650f0d4e2dd7e90857bd692d8f2144076fd
0xakk0r0kamui <hy@autonomous.nyc>
2020-02-24 11:39:04 +0700

    Add more log for bft msg

--- process/datahandler/bft.go
+++ process/datahandler/bft.go
@@ -1,11 +1,22 @@
 package datahandler
 
-import libp2p "github.com/libp2p/go-libp2p-pubsub"
+import (
+	"highway/common"
+
+	libp2p "github.com/libp2p/go-libp2p-pubsub"
+)
 
 type BFTHandler struct {
 }
 
 func (handler *BFTHandler) HandleDataFromTopic(topicReceived string, dataReceived libp2p.Message) error {
+	msgbft, err := common.ParseBFTData(string(dataReceived.Data))
+	logger.Infof("[msgbft] Received msg bft, len %v", len(dataReceived.Data))
+	if err != nil {
+		logger.Infof("[msgbft] Can not parse message bft, error %v", err)
+	} else {
+		logger.Infof("[msgbft] received Type: %v, ChainKey %v", msgbft.Type, msgbft.ChainKey)
+	}
 	//Do not need to process cuz inside and outside using the same topic bft-<cid>-
 	return nil
 }
