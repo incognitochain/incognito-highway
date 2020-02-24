937aa6575a0117140c6dfce5b15e8a01421ec55f
0xakk0r0kamui <hy@autonomous.nyc>
2020-02-24 15:24:17 +0700

    Stream all of block type

--- chain/clientstreamutils.go
+++ chain/clientstreamutils.go
@@ -44,3 +44,41 @@ func GetBlocks(
 	go getStreamData(stream, blkRecv)
 	return blkRecv, nil
 }
+func streamData(
+	stream proto.HighwayService_StreamBlockByHeightClient,
+	blkRecv chan interface{},
+) {
+	defer close(blkRecv)
+	for {
+		blkData, err := stream.Recv()
+		if err == io.EOF {
+			logger.Infof("[stream] Return io.EOF %v", stream)
+			break
+		}
+		if err != nil {
+			logger.Infof("[stream] Received err %v %v", stream, err)
+			break
+		}
+		blkRecv <- blkData.GetData()
+	}
+}
+
+func getBlocks(
+	ctx context.Context,
+	c proto.HighwayServiceClient,
+	req *proto.BlockByHeightRequest,
+) (
+	chan interface{},
+	error,
+) {
+	logger.Infof("[stream] Server call Client: Start stream request %v", req)
+	stream, err := c.StreamBlockByHeight(ctx, req)
+	if err != nil {
+		logger.Infof("[stream] Server call Client return error %v", err)
+		return nil, err
+	}
+	logger.Infof("[stream] Server call Client: OK, return stream %v", stream)
+	blkRecv := make(chan interface{})
+	go streamData(stream, blkRecv)
+	return blkRecv, nil
+}
--- chain/streamserver.go
+++ chain/streamserver.go
@@ -41,3 +41,44 @@ func (s *Server) StreamBlockBeaconByHeight(
 	}
 	return nil
 }
+
+func (s *Server) StreamBlockByHeight(
+	req *proto.BlockByHeightRequest,
+	ss proto.HighwayService_StreamBlockByHeightServer,
+) error {
+	ctx, cancel := context.WithTimeout(context.Background(), common.MaxTimePerRequest)
+	defer cancel()
+	serviceClient, _, err := s.m.client.getClientWithBlock(ctx, int(req.GetTo()), req.Heights[len(req.Heights)-1])
+	logger.Infof("[stream] Serve received request, start requesting blocktype %v from cID %v to cID %v another client: specific %v block by height %v -> %v",
+		req.Type,
+		req.From,
+		req.To,
+		req.Specific,
+		req.Heights[0],
+		req.Heights[len(req.Heights)-1],
+	)
+	if err != nil {
+		logger.Errorf("[stream] No serviceClient with  blocktype %v from cID %v to cID %v another client: specific %v block by height %v -> %v",
+			req.Type,
+			req.From,
+			req.To,
+			req.Specific,
+			req.Heights[0],
+			req.Heights[len(req.Heights)-1],
+		)
+		return err
+	}
+	blkRecv, err := getBlocks(ctx, serviceClient, req)
+	if err != nil {
+		logger.Infof("[stream] Calling client but received error %v, return", err)
+		return err
+	}
+	for blk := range blkRecv {
+		logger.Infof("[stream] Received block from channel, send to client")
+		if err := ss.Send(&proto.BlockData{Data: blk.([]byte)}); err != nil {
+			logger.Infof("[stream] Trying send to client but received error %v, return and cancel context", err)
+			return err
+		}
+	}
+	return nil
+}
