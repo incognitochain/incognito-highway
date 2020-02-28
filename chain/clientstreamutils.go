7d950455f0aac63a07200f2f175fdc89f3d7e286
0xakk0r0kamui <hy@autonomous.nyc>
2020-02-28 16:53:30 +0700

    Remove unuse code

--- chain/clientstreamutils.go
+++ chain/clientstreamutils.go
@@ -6,44 +6,6 @@ import (
 	"io"
 )
 
-func getStreamData(
-	stream proto.HighwayService_StreamBlockBeaconByHeightClient,
-	blkRecv chan interface{},
-) {
-	defer close(blkRecv)
-	for {
-		blkData, err := stream.Recv()
-		if err == io.EOF {
-			logger.Infof("[stream] Return io.EOF %v", stream)
-			break
-		}
-		if err != nil {
-			logger.Infof("[stream] Received err %v %v", stream, err)
-			break
-		}
-		blkRecv <- blkData.GetData()
-	}
-}
-
-func GetBlocks(
-	ctx context.Context,
-	c proto.HighwayServiceClient,
-	req *proto.GetBlockBeaconByHeightRequest,
-) (
-	chan interface{},
-	error,
-) {
-	logger.Infof("[stream] Server call Client: Start stream request %v %v %v %v", req.GetSpecific(), req.GetFromHeight(), req.GetToHeight(), len(req.GetHeights()))
-	stream, err := c.StreamBlockBeaconByHeight(ctx, req)
-	if err != nil {
-		logger.Infof("[stream] Server call Client return error %v", err)
-		return nil, err
-	}
-	logger.Infof("[stream] Server call Client: OK, return stream %v", stream)
-	blkRecv := make(chan interface{})
-	go getStreamData(stream, blkRecv)
-	return blkRecv, nil
-}
 func streamData(
 	stream proto.HighwayService_StreamBlockByHeightClient,
 	blkRecv chan interface{},
--- chain/streamserver.go
+++ chain/streamserver.go
@@ -6,42 +6,6 @@ import (
 	"highway/proto"
 )
 
-// type StreamServer struct {
-// 	proto.UnimplementedHighwayStreamServiceServer
-// 	blkgetter BlockGetter
-// }
-
-// type BlockGetter interface {
-// 	GetBlocks(req *proto.BlockBeaconByHeightRequest) (chan interface{}, error)
-// }
-
-func (s *Server) StreamBlockBeaconByHeight(
-	req *proto.GetBlockBeaconByHeightRequest,
-	ss proto.HighwayService_StreamBlockBeaconByHeightServer,
-) error {
-	ctx, cancel := context.WithTimeout(context.Background(), common.MaxTimePerRequest)
-	defer cancel()
-	serviceClient, _, err := s.m.client.getClientWithBlock(ctx, int(common.BEACONID), req.GetToHeight())
-	logger.Infof("[stream] Serve received request, start requesting another client: block by height: shard %v -> %v, heights = %v", req.GetFromHeight(), req.GetToHeight(), req.GetHeights())
-	if err != nil {
-		logger.Infof("[stream] No serviceClient with block, shardID = %v, heights = %v, err = %+v", req.GetFromHeight(), req.GetToHeight(), err)
-		return err
-	}
-	blkRecv, err := GetBlocks(ctx, serviceClient, req)
-	if err != nil {
-		logger.Infof("[stream] Calling client but received error %v, return", err)
-		return err
-	}
-	for blk := range blkRecv {
-		logger.Infof("[stream] Received block from channel, send to client")
-		if err := ss.Send(&proto.BlockData{Data: blk.([]byte)}); err != nil {
-			logger.Infof("[stream] Trying send to client but received error %v, return", err)
-			return err
-		}
-	}
-	return nil
-}
-
 func (s *Server) StreamBlockByHeight(
 	req *proto.BlockByHeightRequest,
 	ss proto.HighwayService_StreamBlockByHeightServer,
