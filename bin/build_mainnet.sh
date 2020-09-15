c8fc7608f163c139ead6eefb9dc79e7028042a66
0xakk0r0kamui <hy@autonomous.nyc>
2020-09-15 14:12:07 +0700

    Update file build

--- bin/build_mainnet.sh
+++ bin/build_mainnet.sh
@@ -10,6 +10,6 @@ fi
 cp ../keylist.json .
 
 env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-w' -o highway ../*.go && \
-docker build -t incognitochaintestnet/incognito-highway:${tag} . && \
-docker push incognitochaintestnet/incognito-highway:${tag}
+docker build -t incognitochain/incognito-highway:${tag} . && \
+docker push incognitochain/incognito-highway:${tag}
 
