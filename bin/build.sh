dfb396ba3a21320fc738948a0c325668553e5827
0xakk0r0kamui <hy@autonomous.nyc>
2020-09-15 14:16:44 +0700

    Update keylist.json testnet

--- bin/build.sh
+++ bin/build.sh
@@ -7,7 +7,7 @@ if [ -z "$tag" ]; then
     tag=dev
 fi
 
-cp ../keylist.json .
+cp ../testnet/keylist.json .
 
 env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-w' -o highway ../*.go && \
 docker build -t incognitochaintestnet/incognito-highway:${tag} . && \
