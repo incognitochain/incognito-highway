16e162d7c0054820d230ba15261f5318c425fb55
0xakk0r0kamui <hy@autonomous.nyc>
2021-03-15 01:49:39 +0700

    increase delay time

--- common/constant.go
+++ common/constant.go
@@ -50,7 +50,7 @@ var (
 	BroadcastMsgEnlistTimestep = 1 * time.Minute
 
 	PercentGetFromCache = 80
-	DelayDuration       = 5 * time.Second
+	DelayDuration       = 15 * time.Second
 
 	MaxPSMsgSize = 1 << 23
 )
