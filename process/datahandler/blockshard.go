7896d4c12d1efc76dbc15d0510ee029b8841fae5
0xakk0r0kamui <hy@autonomous.nyc>
2022-06-13 10:46:23 +0700

    blockcast blockshard to shard

--- process/datahandler/blockshard.go
+++ process/datahandler/blockshard.go
@@ -4,6 +4,7 @@ import (
 	"highway/process/topic"
 
 	libp2p "github.com/incognitochain/go-libp2p-pubsub"
+	cCommon "github.com/incognitochain/incognito-chain/common"
 )
 
 type BlkShardHandler struct {
@@ -17,13 +18,15 @@ func (handler *BlkShardHandler) HandleDataFromTopic(topicReceived string, dataRe
 	msgType := topic.GetMsgTypeOfTopic(topicReceived)
 	cID := topic.GetCommitteeIDOfTopic(topicReceived)
 	if handler.FromNode {
+		logger.Infof("[msgblockshard] received block from topic %v, data received %v", topicReceived, cCommon.HashH(dataReceived.Data).String())
 		topicPub := topic.Handler.GetHWPubSubOutSideFromMsg(msgType, cID)
 		topicPubs = append(topicPubs, topicPub)
 	} else {
 		topicPubs = topic.Handler.GetHWPubTopicsFromMsg(msgType, cID)
-		topicPubs = topic.Handler.GetHWPubTopicsFromMsg(msgType, topic.NoCIDInTopic)
+		topicPubs = append(topicPubs, topic.Handler.GetHWPubTopicsFromMsg(msgType, topic.NoCIDInTopic)...)
 	}
 	for _, topicPub := range topicPubs {
+		logger.Debugf("[msgblockshard]\tPublish topic %v", topicPub)
 		err := handler.PubSub.Publish(topicPub, dataReceived.GetData())
 		if err != nil {
 			logger.Errorf("Publish topic %v return error: %v", topicPub, err)
